b84f83e92fc6b51973d30923fefbe789741d03eb
sahil-raj <sahilrajaaru@gmail.com>
2025-02-07 05:13:04 +0530

    chore: cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import routes from "./routes";
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api", routes);
