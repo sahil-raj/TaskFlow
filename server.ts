ddc7c20dde1be614256402845f55137b5eded85d
sahil-raj <sahilrajaaru@gmail.com>
2025-02-07 00:41:45 +0530

    chore: update db config & validation

--- server.ts
+++ server.ts
@@ -1,5 +1,18 @@
 import app from "./src/app";
 import { PORT } from "./src/config/env";
+import sequelize from "./src/config/db";
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Connected to Supabase Postgres via Sequelize"))
+  .catch((err) => console.error("Sequelize connection error:", err));
+
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Database synced successfully!");
+  })
+  .catch((err) => console.error("Database sync error:", err));
 
 app.listen(PORT, () => {
   console.log(`server running on port: ${PORT}`);
---
+++ src/config/db.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize";
+import { DB_URL } from "./env";
+
+const sequelize = new Sequelize(DB_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+export default sequelize;
