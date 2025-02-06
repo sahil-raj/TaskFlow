521836bb1eeed666e457fa4b7c69d9868bdb6a6e
sahil-raj <sahilrajaaru@gmail.com>
2025-02-07 03:15:24 +0530

    chore: update, delete and create

--- src/routes/task_routes.ts
+++ src/routes/task_routes.ts
@@ -54,4 +54,63 @@ router.get("/user_id/:id", async (req: Request, res: Response) => {
   }
 });
 
+//create new task
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    //add check for user
+    const task = await Task.create(req.body);
+    res.status(200).json(task);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+
+//delete
+router.delete("/id/:id", async (req: Request, res: Response) => {
+  const taskId = req.params?.id;
+  if (!taskId || taskId == "") {
+    res.status(400).json({ message: "task id is required" });
+  } else {
+    try {
+      const task = await Task.findByPk(taskId);
+
+      if (!task) {
+        res.status(404).json({ message: "task not found with this id" });
+        return;
+      }
+
+      await task?.destroy();
+      res.status(200).json({ message: "task deleted" });
+    } catch (e) {
+      res.json(500).json(e);
+    }
+  }
+});
+
+//update task
+router.put("/id/:id", async (req: Request, res: Response) => {
+  const taskId = req.params?.id;
+  if (!taskId || taskId == "") {
+    res.status(400).json({ message: "task id is required" });
+  } else {
+    try {
+      const task = await Task.findByPk(taskId);
+      if (!task) {
+        res.status(404).json({ message: "task not found" });
+        return;
+      }
+
+      await Task.update(req.body, {
+        where: {
+          id: taskId,
+        },
+      });
+
+      res.status(200).json({ message: "task updated" });
+    } catch (e) {
+      res.status(400).json(e);
+    }
+  }
+});
+
 export default router;
